d2203c64fec7618a809f25e886f8e1292973dd89
techieshubham30 <82032825+techieshubham30@users.noreply.github.com>
2022-01-10 17:44:20 +0530

    initial commit

--- operators_branching_loops/Intermediate/question4.js
+++ operators_branching_loops/Intermediate/question4.js
@@ -1,29 +1,16 @@
 // Write a program to return a Fibonacci series : 0,1,1,2,3,5,8,13,21....
 
 
-var readlineSync = require('readline-sync');
 
-var num = readlineSync.question('Enter a number ');
+const fibonacciSeries=(n)=>{
+ let [firstNum,secondNum] = [0,1];
+ let fibonacciArr = [0,1];
+ for(let i=2;i<n;i++){
+   fibonacciArr.push(fibonacciArr[i-1]+fibonacciArr[i-2]);
+    
+ }
+ return fibonacciArr;
 
-var x=fibonacciSeries(num);
-for(var i=0;i<x.length;i++){
-  console.log(x[i]);
 }
-function fibonacciSeries(n){
-  var firstNo=0;
-  var secondNo=1;
-  var fibonacciArray=[];
-  fibonacciArray[0]=firstNo;
-  fibonacciArray[1]=secondNo;
 
-  for(var i=2;i<n;i++){
-    fibonacciArray[i]=firstNo+secondNo;
-    firstNo=secondNo;
-    secondNo=fibonacciArray[i];
-
-  }
-  return fibonacciArray;
-
-  }
-
-
+console.log(fibonacciSeries(10));
